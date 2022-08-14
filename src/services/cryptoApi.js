8ae9d15456da9d2abf1a21796fd2e5d8beea3248
frederick-guo <freddyguo07@gmail.com>
2022-08-14 18:22:10 -0500

    added cryptocurrencies page

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '9ac155200bmsh438043a1e33fc40p1c52b4jsna31c588ce68f'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
